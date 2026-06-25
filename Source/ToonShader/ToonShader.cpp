2548383d87179a210a7978c6d3fd4f210325eca4
luolaniii <2099457264@qq.com>
2026-06-25 07:08:11 -0500

    Add UE toon shader project

---
+++ Source/ToonShader/ToonShader.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ToonShader.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ToonShader, "ToonShader" );
